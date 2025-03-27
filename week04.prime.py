ca09ce0178b3e336342b14145e976b3e88cc469c
202130317 <wonseo>
2025-03-27 12:14:04 +0900

    v0.5 prime number

--- week04.prime.py
+++ week04.prime.py
@@ -7,6 +7,7 @@ The number to determine whether it is prime or not
     """
 
 
+
 number = int(input("숫자를 입력하세요: "))
 is_prime = True
 
@@ -14,12 +15,13 @@ if number >= 2:  # 숫자가 2 이상인 경우에만 검사
     i = 2
     while i * i <= number:
         if number % i == 0:
-            is_prime = False  # 소수가 아님을 표시
+            retrun = False  # 소수가 아님을 표시
         print(i, end=" ")  # 약수 출력
         i = i + 1  # 항상 i를 증가시킴
 else:
-    is_prime = False  # 숫자가 2 미만인 경우 소수가 아님
+    retrun = False  # 숫자가 2 미만인 경우 소수가 아님
 
+n = int(input())
 if is_prime:
     print(f"\n{number}는(은) 소수입니다.")
 else:
